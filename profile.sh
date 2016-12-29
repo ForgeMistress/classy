cd00699a0e66f801b6117cf675903d047ead677b
WildPyromancer <pyro@nospam.com>
2016-12-28 23:22:24 -0500

    Adding profile results and code.

--- profile.sh
+++ profile.sh
@@ -17,12 +17,10 @@ if [[ $HOSTTYPE == x86_64 ]]; then
     arch=x64
 fi
 
-luarunnerdir=bin/$PLATFORM/luajit-2.0.4-x64
-luarunner=luajit
-
 # Set this to whatever you use to call a lua runtime on the command line. I've been using a locally built luajit 
-# runtime for mine.
-lua=$luarunnerdir/$luarunner
+# runtime for mine. I've also been using Love2D compiled against Luajit 2.1-beta2 as well as Luapower, but that's 
+# neither here nor there.
+lua=luajit
 
 startdir=$PWD
 
@@ -38,41 +36,41 @@ done
 iterations=500000
 
 echo Profiling Classy Classes Memory
-$lua Profiler/profile.lua classy classes $iterations
+$lua profile.lua classy classes $iterations
 echo
 
 echo Profiling Middleclass Classes Memory
-$lua Profiler/profile.lua middleclass classes $iterations
+$lua profile.lua middleclass classes $iterations
 echo
 
 echo Profiling Classy Allocation
-$lua Profiler/profile.lua classy allocation $iterations
+$lua profile.lua classy allocation $iterations
 echo
 
 echo Profiling Middleclass Allocation
-$lua Profiler/profile.lua middleclass allocation $iterations
+$lua profile.lua middleclass allocation $iterations
 echo
 
 echo Profiling Classy Method Invocation
-$lua Profiler/profile.lua classy methods $iterations
+$lua profile.lua classy methods $iterations
 echo
 
 echo Profiling Middleclass Method Invocation
-$lua Profiler/profile.lua middleclass methods $iterations
+$lua profile.lua middleclass methods $iterations
 echo
 
 echo Profiling Classy Subclass Allocation
-$lua Profiler/profile.lua classy inheritance-allocation $iterations
+$lua profile.lua classy inheritance-allocation $iterations
 echo
 
 echo Profiling Middleclass Subclass Allocation
-$lua Profiler/profile.lua middleclass inheritance-allocation $iterations
+$lua profile.lua middleclass inheritance-allocation $iterations
 echo
 
 echo Profiling Classy Subclass Method Invocation
-$lua Profiler/profile.lua classy inheritance-methods $iterations
+$lua profile.lua classy inheritance-methods $iterations
 echo
 
 echo Profiling Middleclass Subclass Method Invocation
-$lua Profiler/profile.lua middleclass inheritance-methods $iterations
+$lua profile.lua middleclass inheritance-methods $iterations
 echo
